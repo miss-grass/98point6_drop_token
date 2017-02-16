1253531a559ce8c6cab76c253cedaf7290a45919
miss-grass <liz67@uw.edu>
2017-02-15 18:01:38 -0800

    Add files via upload

---
+++ GameMain.java
@@ -0,0 +1,76 @@
+// Zhu Li
+// 2/15/17
+//
+// Class GameMain is the driver program for the game management task.
+// It reads comments from users, prints outputs to the users until the game is called over.
+
+import java.util.Scanner;
+
+public class GameMain {
+	private static final int DIMENSION = 4;  // dimension of the board
+
+	public static void main(String[] args) {
+		// give information
+		System.out.println("Welcome to 98point6 Drop Token!");
+		instruction();
+		
+		// create new GameManage
+        GameManage game = new GameManage();
+
+        // set basic parameters
+        Scanner console = new Scanner(System.in);
+        String command = console.nextLine();
+        int player = 0; // 0 indicates player 1, and 1 indicates player 2
+        
+        // loop over stdln taking commands, stop when "EXIT"
+        while (!command.equalsIgnoreCase("EXIT")) {   
+        	Scanner line = new Scanner(command);
+        	String word = line.next();
+        	if (word.equalsIgnoreCase("PUT")) {
+        		int col = line.nextInt();
+                if (col < 1 || col > DIMENSION) {  // deal with invalid input
+        			System.out.println("Invalid column number! (1 <= <COLUMN> <= 4)");
+        		} else {
+	        		String output = game.put(col, player+1);
+	        		System.out.println(output);
+	        		if (!output.equals("ERROR")) {
+	        			player = 1 - player;
+	        		}	
+        		}
+        	} else if (word.equalsIgnoreCase("BOARD")) {
+        		int[] board = game.board();
+        		//System.out.println(board.length);
+        		for (int i = 0; i <= board.length - DIMENSION; i += DIMENSION) {
+        			System.out.print("|");
+        			for (int j = 0; j < DIMENSION; j++) {
+        				System.out.print(" " + board[(j + i)]);
+        			}
+        			System.out.println();    			
+        		}
+        		System.out.println("+--------");
+        		System.out.println("  1 2 3 4");
+        	} else if (word.equalsIgnoreCase("GET")) {
+        		int[] list = game.get();
+        		for (int i = 0; i < game.size(); i++) {
+        			System.out.println(list[i]);
+        		}
+        	} else {
+        		// gives instructions if gets invalid input
+        		System.out.println("Invalid Input!");
+        		instruction();       		
+        	}
+        	// continue reading input
+            command = console.nextLine();
+        }
+	}
+	
+	// prints instructions of valid commands
+	public static void instruction() {
+		System.out.println("    PUT <COLUMN> : drop a token along column <COLUMN> (1 <= <COLUMN> <= 4)");
+		System.out.println("    GET : show a list of columns that have been successfully put to");
+		System.out.println("    BOARD : show current board state");
+		System.out.println("    EXIT : ends the game");
+	}
+}
+
+
